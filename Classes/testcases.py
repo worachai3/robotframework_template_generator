c90d1b59d1e914fe009eff0f85132e4a0d93bec3
worachai.wuttiworachairung <worachai3@live.com>
2022-01-17 11:22:42 +0700

    Add func for adding newline in test case name

--- Classes/testcases.py
+++ Classes/testcases.py
@@ -44,16 +44,38 @@ class Testcases(Base):
         self.found_tags = False
         self.found_tags_first_row = False
 
-    def __is_testcase_generated(self, testcase_no):
-        if testcase_no in self.generated_testcases:
-            self.found_testcase = False
-            self.found_new_testcase = False
-            return True
-        return False
+    def __set_variables_when_found_new_testcase_number(self, line):
+        if self.__found_testcases_number(line):
+            if self.found_testcase:
+                self.found_testcase = False
+                self.found_new_testcase = True
+            else:
+                self.found_testcase = True
+
+    def __is_not_found_testcase_or_is_not_end_of_testcase(self):
+        return not self.found_testcase and not self.found_new_testcase
 
     def __is_end_of_documentation_or_tags(self, line):
         if (self.found_documentation_first_row or self.found_tags_first_row):
             return line.startswith('    ...')
+
+    def __is_end_of_testcases_section(self, line):
+        return line.startswith('***')
+
+    def __is_documentation(self, line):
+        return line.startswith('    [Documentation]')
+
+    def __is_tags(self, line):
+        return line.startswith('    [Tags]')
+
+    def __is_testcase_section(self, line):
+        return self.match(line, '*** test cases ***')
+
+    def __is_name_too_long(self, name):
+        return len(name) >= 120
+
+    def __is_testcase_generated(self, testcase_no):
+        return testcase_no in self.generated_testcases
 
     def __add_priority_to_tag_str(self, tag_str, priority):
         tag_str += '    ' + priority
@@ -82,9 +104,6 @@ class Testcases(Base):
         tag_str = self.__add_defect_to_tag_str(tag_str, row[defects])
         self.script.append(tag_str)
 
-    def __found_testcases_number(self, line):
-        return re.search('^[A-Za-z]+-[0-9]+$', line)
-
     def __append_testcase_number_to_list(self, line):
         self.found_testcase = True
         self.script.append(line)
@@ -93,59 +112,60 @@ class Testcases(Base):
     def __append_documentation_to_list(self, row):
         self.found_documentation = True
         self.found_documentation_first_row = True
-        self.script.append('    [Documentation]    ' + row[tc_name])
+        self.script.append(self.__get_testcase_name_string(row[tc_name]))
 
     def __append_tags_to_list(self, row):
         if not self.found_documentation:
             self.found_documentation = True
-            self.script.append(
-                '    [Documentation]    ' + row[tc_name])
+            self.script.append(self.__get_testcase_name_string(row[tc_name]))
         self.found_tags = True
         self.found_tags_first_row = True
         self.__add_tag(row)
 
-    def __exceptional_case(self, row):
-        if not self.found_documentation:
-            self.found_documentation = True
-            self.script.append('    [Documentation]    ' + row[tc_name])
-        if not self.found_tags:
-            self.found_tags = True
-            self.__add_tag(row)
-
-    def __is_end_of_testcases_section(self, line):
-        return line.startswith('***')
+    def __append_space_to_last_element_to_list(self):
+        if self.script:
+            if self.script[-1].strip() != '':
+                self.__append_to_list('')
 
     def __append_to_list(self, line):
         self.script.append(line)
         self.found_documentation_first_row = False
         self.found_tags_first_row = False
 
-    def __is_documentation(self, line):
-        return line.startswith('    [Documentation]')
+    def __exceptional_case(self, row):
+        if not self.found_documentation:
+            self.found_documentation = True
+            self.script.append(self.__get_testcase_name_string(row[tc_name]))
+        if not self.found_tags:
+            self.found_tags = True
+            self.__add_tag(row)
 
-    def __is_tags(self, line):
-        return line.startswith('    [Tags]')
+    def __found_testcases_number(self, line):
+        return re.search('^[A-Za-z]+-[0-9]+$', line)
 
-    def __is_testcase_section(self, line):
-        return self.match(line, '*** test cases ***')
-
-    def __set_variables_when_found_new_testcase_number(self, line):
-        if re.search('^[A-Za-z]+-[0-9]+$', line):
-            if self.found_testcase:
-                self.found_testcase = False
-                self.found_new_testcase = True
+    def __get_testcase_name_string(self, testcase_name):
+        res = ''
+        line = '    [Documentation]    '
+        testcase_name_list = testcase_name.split()
+        for index in range(len(testcase_name_list)):
+            if not self.__is_name_too_long(line + testcase_name_list[index]):
+                line += testcase_name_list[index].strip() + ' '
+                res += testcase_name_list[index].strip() + ' '
             else:
-                self.found_testcase = True
+                if testcase_name_list[index] != testcase_name_list[-1]:
+                    line = '    ...    '
+                    res += '\n    ...    '
+        res = self.__get_documentation_string() + res
+        return res
 
-    def __not_found_testcase_or_not_end_of_testcase(self):
-        if not self.found_testcase and not self.found_new_testcase:
-            return True
-        return False
+    def __get_documentation_string(self):
+        return '    ' + '[Documentation]' + '    '
 
     def __gen_new_testcase(self, row):
         self.script.append(row[tc_no])
-        self.script.append('    ' + '[Documentation]' + '    ' + row[tc_name])
+        self.script.append(self.__get_testcase_name_string(row[tc_name]))
         self.__add_tag(row)
+        self.script.append('    Log To Console    EMPTY TEST CASE SCRIPT')
         self.script.append('')
 
     def __find_testcase_script_from_testcases_row(self, row):
@@ -176,6 +196,7 @@ class Testcases(Base):
             if self.__is_end_of_testcases_section(line):
                 break
             self.__append_to_list(line)
+        self.__append_space_to_last_element_to_list()
         old_robot_file.close()
 
     def __find_testcases_script_from_testcases_file(self, testcases_file_path):
@@ -197,9 +218,11 @@ class Testcases(Base):
             if not self.found_testcases_section:
                 continue
             self.__set_variables_when_found_new_testcase_number(line)
-            if self.__not_found_testcase_or_not_end_of_testcase():
+            if self.__is_not_found_testcase_or_is_not_end_of_testcase():
                 continue
             if self.__is_testcase_generated(line):
+                self.found_testcase = False
+                self.found_new_testcase = False
                 continue
             if self.__is_end_of_testcases_section(line):
                 break
