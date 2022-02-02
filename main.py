17429e7b2e985f9b128bcd5c32dddc5e8aba7b74
Worachai Wuttiworachairung <worachai.wuttiworachairung@scb.coth>
2022-02-02 18:10:03 +0700

    Adjust format

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QPushButton, QCheckBox, QTextEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QPushButton, QCheckBox, QTextEdit, QLabel
 import PyQt5.QtGui as QtGui
 
 
@@ -11,54 +11,13 @@ class App(QWidget):
         self.title = 'Robot Template Gen'
         self.left = 10
         self.top = 10
-        self.width = 1100
-        self.height = 200
+        self.width = 1200
+        self.height = 125
+        self.setFixedSize(self.width, self.height)
         self.initUI()
         self.old_robot_file_path = ''
         self.new_robot_file_path = ''
         self.testcase_path = ''
-        self.option_checkbox = QCheckBox(
-            'Merge tags in test cases file with old script', self)
-        self.option_checkbox.setGeometry(5, 90, 300, 20)
-
-        self.new_script_file_path_text = QTextEdit(self)
-        self.old_script_file_path_text = QTextEdit(self)
-        self.testcase_file_path_text = QTextEdit(self)
-
-        self.new_script_file_path_text.setReadOnly(True)
-        self.old_script_file_path_text.setReadOnly(True)
-        self.testcase_file_path_text.setReadOnly(True)
-        self.new_script_file_path_text.setFont(QtGui.QFont('Arial', 10))
-        self.old_script_file_path_text.setFont(QtGui.QFont('Arial', 10))
-        self.testcase_file_path_text.setFont(QtGui.QFont('Arial', 10))
-
-        # font = QtGui.QFont()
-        # font.setPointSize(16)
-        # self.editor.setFont(font)
-
-        self.testcase_file_path_text.setGeometry(90, 0, 1005, 30)
-        self.old_script_file_path_text.setGeometry(90, 30, 1005, 30)
-        self.new_script_file_path_text.setGeometry(90, 60, 1005, 30)
-
-        browse_old_script_button = QPushButton('Old Script', self)
-        browse_old_script_button.setGeometry(0, 30, 90, 30)
-        browse_old_script_button.clicked.connect(
-            lambda: self.get_old_robot_script())
-
-        browse_new_script_button = QPushButton('New Script', self)
-        browse_new_script_button.setGeometry(0, 60, 90, 30)
-        browse_new_script_button.clicked.connect(
-            lambda: self.get_new_robot_script())
-
-        browse_test_case_button = QPushButton('Test Cases', self)
-        browse_test_case_button.setGeometry(0, 0, 90, 30)
-        browse_test_case_button.clicked.connect(
-            lambda: self.get_test_case_file())
-
-        generate_new_file_button = QPushButton('Generate', self)
-        generate_new_file_button.setGeometry(0, 110, 1100, 90)
-        generate_new_file_button.clicked.connect(
-            lambda: self.run_python_script())
 
         self.show()
 
@@ -73,6 +32,54 @@ class App(QWidget):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        self.option_checkbox = QCheckBox(
+            'Merge tags in test cases file with old script', self)
+        self.option_checkbox.setGeometry(5, 95, 300, 20)
+
+        self.new_script_file_path_text = QTextEdit(self)
+        self.old_script_file_path_text = QTextEdit(self)
+        self.testcase_file_path_text = QTextEdit(self)
+
+        self.new_script_file_path_text.setReadOnly(True)
+        self.old_script_file_path_text.setReadOnly(True)
+        self.testcase_file_path_text.setReadOnly(True)
+
+        self.new_script_file_path_text.setFont(QtGui.QFont('Arial', 10))
+        self.old_script_file_path_text.setFont(QtGui.QFont('Arial', 10))
+        self.testcase_file_path_text.setFont(QtGui.QFont('Arial', 10))
+
+        self.testcase_file_path_text.setGeometry(90, 0, 1005, 30)
+        self.old_script_file_path_text.setGeometry(90, 30, 1005, 30)
+        self.new_script_file_path_text.setGeometry(90, 60, 1005, 30)
+
+        self.old_script_file_path_label = QLabel(self)
+        self.old_script_file_path_label.setText('Old Script')
+        self.old_script_file_path_label.setGeometry(10, 30, 90, 30)
+        self.browse_old_script_button = QPushButton('Browse', self)
+        self.browse_old_script_button.setGeometry(1100, 30, 90, 30)
+        self.browse_old_script_button.clicked.connect(
+            lambda: self.get_old_robot_script())
+
+        self.new_script_file_path_label = QLabel(self)
+        self.new_script_file_path_label.setText('New Script')
+        self.new_script_file_path_label.setGeometry(10, 60, 90, 30)
+        self.browse_new_script_button = QPushButton('Browse', self)
+        self.browse_new_script_button.setGeometry(1100, 60, 90, 30)
+        self.browse_new_script_button.clicked.connect(
+            lambda: self.get_new_robot_script())
+
+        self.test_case_file_path_label = QLabel(self)
+        self.test_case_file_path_label.setText('Test Cases')
+        self.test_case_file_path_label.setGeometry(10, 0, 90, 30)
+        self.browse_test_case_button = QPushButton('Browse', self)
+        self.browse_test_case_button.setGeometry(1100, 0, 90, 30)
+        self.browse_test_case_button.clicked.connect(
+            lambda: self.get_test_case_file())
+
+        self.generate_new_file_button = QPushButton('Generate', self)
+        self.generate_new_file_button.setGeometry(550, 92, 90, 30)
+        self.generate_new_file_button.clicked.connect(
+            lambda: self.run_python_script())
 
     def get_old_robot_script(self):
         options = QFileDialog.Options()
